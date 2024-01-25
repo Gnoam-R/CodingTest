f0a3737d9b81a0196d32518159617c94b8a2d180
Hyung Woo Roh <67363759+Gnoam-R@users.noreply.github.com>
2024-01-25 15:14:23 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.swift
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.swift
@@ -1,15 +1,3 @@
 func solution(_ s:String) -> Int {
-    var input = s
-    var result = 0
-    if input.first == "-" {
-        result = Int(s)!
-    }
-    // else if input.first == "+" {
-    //     input.removeFirst()
-    //     result = Int(s)!
-    // }
-    else {
-        result = Int(s)!
-    }
-    return result
+    return Int(s)!
 }
